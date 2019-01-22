3c300f4cabe82214c944b22598a51de2bd2e1b40
Peeratham Techapalokul <tpeera4@vt.edu>
2019-01-22 11:55:35 -0500

    wip show hint example

--- src/lib/quality-tutor-hoc.jsx
+++ src/lib/quality-tutor-hoc.jsx
@@ -7,6 +7,19 @@ const qualityTutorHOC = function (WrappedComponent) {
     class QualityTutor extends React.Component {
         constructor(props){
             super(props);
+            this.hints = [];
+        }
+
+        mockHintGeneration() {
+            const workspace = ScratchBlocks.getMainWorkspace();
+            const blockIds = Object.keys(workspace.blockDB_);
+            if(blockIds.length>0){
+                const block = workspace.getBlockById(blockIds[0]); 
+                if (!block.isShadow_ && !block.hint) { 
+                    block.setHintText("");
+                    block.hint.setVisible(true);
+                }
+            }
         }
 
         componentDidMount () {
@@ -15,13 +28,23 @@ const qualityTutorHOC = function (WrappedComponent) {
 
         initializeTutor () {
             console.log('initialize tutor');
+            const vm = this.props.vm;
             const workspace = ScratchBlocks.getMainWorkspace();
+
+            this.blockListener = this.blockListener.bind(this);
+            workspace.addChangeListener(this.blockListener);
             
             new Promise( (resolve, reject) => {
                 resolve(Blockly.Xml.workspaceToDom(workspace));
             } ).then(dom =>{
                 console.log(Blockly.Xml.domToText(dom));
             });
+        }
+
+        blockListener (e) {
+            console.log('TODO: listen for ui related to hint');
+            console.log(e);
+            this.mockHintGeneration();
         }
 
         render () {
